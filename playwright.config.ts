e5c85d4b5835d6446e0c2c141669a02061aa954f
Vitaliy Potapov <noginsk@rambler.ru>
2023-07-26 18:43:17 +0400

    setup several projects to reproduce playwright-bdd#39

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,23 +1,40 @@
 import { defineConfig, devices } from '@playwright/test';
 import { defineBddConfig } from 'playwright-bdd';
 
-const testDir = defineBddConfig({
-  importTestFrom: 'steps/fixtures.ts',
-  paths: ['features'],
-  require: ['steps/*.ts'],
-});
-
 export default defineConfig({
-  testDir,
   reporter: 'html',
   projects: [
     {
-      name: 'chromium',
+      name: 'Login setup',
+      testDir: defineBddConfig({
+        outputDir: '.features-gen/login',
+        importTestFrom: 'steps/fixtures.ts',
+        paths: ['features'],
+        require: ['steps/*.ts'],
+      }),
       use: { ...devices['Desktop Chrome'] },
     },
     {
+      name: 'chromium',
+      testDir: defineBddConfig({
+        outputDir: '.features-gen/regression-chrome',
+        importTestFrom: 'steps/fixtures.ts',
+        paths: ['features'],
+        require: ['steps/*.ts'],
+      }),
+      use: { ...devices['Desktop Chrome'] },
+      dependencies: ['Login setup']
+    },
+    {
       name: 'firefox',
+      testDir: defineBddConfig({
+        outputDir: '.features-gen/regression-firefox',
+        importTestFrom: 'steps/fixtures.ts',
+        paths: ['features'],
+        require: ['steps/*.ts'],
+      }),
       use: { ...devices['Desktop Firefox'] },
+      dependencies: ['Login setup']
     },
   ]
 });
