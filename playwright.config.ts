10dd0d50b6a4a350f3a9fda5b3333b32e3687553
Vitaliy Potapov <noginsk@rambler.ru>
2024-07-04 00:17:16 +0400

    upd pw-bdd to v7

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig, devices } from '@playwright/test';
-import { defineBddConfig, cucumberReporter } from 'playwright-bdd';
+import { defineConfig, devices } from "@playwright/test";
+import { defineBddConfig, cucumberReporter } from "playwright-bdd";
 
 const testDir = defineBddConfig({
-  paths: ['features/*.feature'],
-  require: ['steps/*.ts'],
-  importTestFrom: 'steps/fixtures.ts',
-  quotes: 'single',
+  features: "features/*.feature",
+  steps: "steps/*.ts",
 });
 
 export default defineConfig({
   testDir,
-  reporter: [cucumberReporter('html', { outputFile: 'cucumber-report/report.html' })],
+  reporter: [
+    cucumberReporter("html", { outputFile: "cucumber-report/report.html" }),
+  ],
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
-  ]
+  ],
 });
