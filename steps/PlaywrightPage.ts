cc202dd6aaa37f2bdfc633c338d04492861dc35f
Vitaliy Potapov <noginsk@rambler.ru>
2024-09-26 11:31:05 +0400

    upd pw-bdd

--- steps/PlaywrightPage.ts
+++ steps/PlaywrightPage.ts
@@ -1,23 +1,23 @@
-import { Page, expect } from '@playwright/test';
-import { Fixture, Given, When } from 'playwright-bdd/decorators';
-import type { test } from './fixtures';
+import { Page, expect } from "@playwright/test";
+import { Fixture, Given, When } from "playwright-bdd/decorators";
+import type { test } from "./fixtures";
 
 export
-@Fixture<typeof test>('playwrightPage')
+@Fixture<typeof test>("playwrightPage")
 class PlaywrightPage {
   constructor(public page: Page) {}
 
-  @Given('I am on Playwright home page')
+  @Given("I am on Playwright home page")
   async open() {
-    await this.page.goto('https://playwright.dev');
+    await this.page.goto("https://playwright.dev");
   }
 
-  @When('I click link {string}')
+  @When("I click link {string}")
   async clickLink(name: string) {
-    await this.page.getByRole('link', { name }).click();
+    await this.page.getByRole("link", { name }).click();
   }
 
-  @When('I see in title {string}')
+  @When("I see in title {string}")
   async assertTitle(text: string) {
     await expect(this.page).toHaveTitle(new RegExp(text));
   }
