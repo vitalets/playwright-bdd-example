24c74f5f15f40539a32144b12d7a055e2fa16090
vitalets <noginsk@rambler.ru>
2025-02-21 18:39:37 +0400

    chromatic works

--- features/steps/fixtures.ts
+++ features/steps/fixtures.ts
@@ -1,10 +1,12 @@
+import { mergeTests } from '@playwright/test';
 import { test as base, createBdd } from 'playwright-bdd';
+import { test as chromaticTest } from '@chromatic-com/playwright';
 
 type Fixtures = {
   // set types of your fixtures
 };
 
-export const test = base.extend<Fixtures>({
+export const test = mergeTests(chromaticTest, base).extend<Fixtures>({
   // add your fixtures
 });
 
