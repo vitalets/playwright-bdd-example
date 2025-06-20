14a6a84c04a034368c5ae44ca902a2fc36aea565
duraisankar.k <duraisankar.k@zohocorp.com>
2025-06-20 13:35:51 +0530

    repro for playwright-bdd issue 318

--- features/steps/index.ts
+++ features/steps/index.ts
@@ -5,10 +5,6 @@ Given('I am on Playwright home page', async ({ page }) => {
   await page.goto('https://playwright.dev');
 });
 
-When('I click link {string}', async ({ page }, name: string) => {
-  await page.getByRole('link', { name }).click();
-});
-
 Then('I see in title {string}', async ({ page }, text: string) => {
   await expect(page).toHaveTitle(new RegExp(text));
 });
