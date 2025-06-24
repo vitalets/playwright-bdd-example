cd1a32cc4514291e355bfb9e5a48f837facc044e
vitalets <noginsk@rambler.ru>
2025-06-24 18:51:54 +0400

    setup reports merge

--- features/homepage2.feature
+++ features/homepage2.feature
@@ -1,6 +1,6 @@
-Feature: Playwright Home Page
+Feature: Playwright Home Page 2
 
-  Scenario: Check title
+  Scenario: Check title 2
     Given I am on Playwright home page
     When I click link "Get started"
     Then I see in title "Installation"
--- playwright.config.ts
+++ playwright.config.ts
@@ -6,15 +6,20 @@ const testDir = defineBddConfig({
   steps: 'features/steps/*.ts',
 });
 
+// disable the removal of the blob report directory for shard runs
+process.env.PWTEST_BLOB_DO_NOT_REMOVE = '1';
+
+const isShardRun = process.argv.some((a) => a.startsWith('--shard'));
+
 export default defineConfig({
   testDir,
-  reporter: [
-    cucumberReporter('html', {
-      outputFile: 'cucumber-report/index.html',
-      externalAttachments: true,
-    }),
-    ['html', { open: 'never' }],
-  ],
+  reporter: isShardRun 
+    ? [['blob', { outputDir: `./output/blob-report`}]]
+    : [
+      cucumberReporter('html', { outputFile: './output/cucumber-report/index.html' }),
+      cucumberReporter('junit', { outputFile: './output/cucumber-report/report.xml' }),
+      cucumberReporter('json', { outputFile: './output/cucumber-report/report.json' }),
+    ],
   use: {
     screenshot: 'on',
     trace: 'on',
